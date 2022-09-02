31cdee4c8497fecc503d024cfd75256958f043d7
Cristhian Castro <cristhian.castro.lucas@udla.edu.ec>
2022-09-02 03:16:59 -0500

    third

--- src/utils/GameList.js
+++ src/utils/GameList.js
@@ -4,11 +4,11 @@ const games=[
     // {name:'kart',id:3,image:'kart.jpg'},
     // {name:'lego',id:4,image:'lego.jpg'},
     {name:'drive_test',
-    display_name:"Drive Test",
+    display_name:"Test Psicométrico: Control de Coches Doble",
         id:5,
         image:'drive_test.jpg',
-        short:'Pon a prueba tus habilidades con este test donde controlarás dos coches que aceleran automáticamente hasta su destino, tu objetivo es evitar que estos choquen',
-        description:'Pon a prueba tus habilidades con este test donde controlarás dos coches que aceleran automáticamente hasta su destino, tu objetivo es evitar que estos choquen. Cada uno tiene su propio carril del cual no pueden salir. ¿Podrás mantenerlos en el camino hasta que el tiempo se acabe?'
+        short:'Pon a prueba tus habilidades de concentración con este test psicométrico donde controlarás dos coches que aceleran automáticamente hasta su destino, tu objetivo es evitar que estos choquen',
+        description:'Pon a prueba tus habilidades de concentración con este test donde controlarás dos coches que aceleran automáticamente hasta su destino, tu objetivo es evitar que estos choquen. Cada uno tiene su propio carril del cual no pueden salir. ¿Podrás mantenerlos en el camino hasta que el tiempo se acabe?'
     }]
 
 export function getList(){
